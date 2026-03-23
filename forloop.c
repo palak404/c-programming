b98360184c8e09603708427878e227631a82a3a5
Palak <palakchauhan8958@gmail.com>
2026-03-23 17:11:58 +0530

    add file

---
+++ forloop.c
@@ -0,0 +1,214 @@
+//Q1. program to demonstrate for statement to display numbers from 0 to 9
+
+/*#include<stdio.h>
+void main()
+{
+    int i;
+    for(i=0; i<=9; i++)
+    {
+        printf("%d\n", i);
+    }
+}*/
+
+//Q2. program to demonstrate for statement to display numbers from 0 to 10
+
+/*#include<stdio.h>
+void main()
+{
+    int i;
+    for(i=0; i<=10; i++)
+    {
+        printf("%d\n", i);
+    }
+}*/
+
+
+//Q3. program to display numbers from 9 to 0 (in reverse order) using for statement-
+
+/*#include<stdio.h>
+void main()
+{
+    int i;
+    for(i=9; i>=0; i--)
+    {
+        printf("%d\n", i);
+    }
+}*/
+
+//Q4. program to display numbers from 10 to 0 (in reverse order) using for statement
+
+/*#include<stdio.h>
+void main()
+{
+    int i;
+    for(i=10; i>=0; i--)
+    {
+        printf("%d\n", i);
+    }
+}*/
+
+
+//Q5. program to display numbers 0 to 10 with a gap of 2 between the numbers using for statement-
+
+/*#include<stdio.h>
+void main()
+{
+    int i;
+    for(i=0; i<=10; i += 2)
+    {
+        printf("%d\n",i);
+    }
+    
+}*/
+
+//Q6. program to display numbers 0 to 50 with a gap of 5 between the numbers using for statement
+
+/*#include<stdio.h>
+void main()
+{
+    int i;
+    for(i=0; i<=50; i += 5)
+    {
+        printf("%d\n",i);
+    }
+}*/
+
+//Q7. program to display numbers 50 to 1 (in reverse order) with a gap of 5 between the numbers using for statement
+
+
+/*#include<stdio.h>
+void main()
+{
+    int i;
+    for(i=50; i>=1; i -= 5)
+    {
+        printf("%d\n",i);
+    }
+}*/
+
+
+//Q8. program to find sum of numbers from 1 to 10 using for statement -
+ 
+/*#include<stdio.h>
+void main()
+{
+    int i;
+    int sum=0;
+    for(i=1; i<=10; i++)
+    {
+       sum=sum+i;
+    }
+   printf("%d\n",sum);
+}*/
+
+//Q9. program to find sum of even numbers from 1 to 10 using for statement
+
+/*#include<stdio.h>
+void main()
+{
+    int i;
+    int sum=0;
+    for(i=1; i<=10; i++)
+    {
+        if(i%2==0)
+    
+    {
+       sum=sum+i;
+    }
+  
+}
+ printf("%d\n",sum);
+}*/
+
+//Q10. program to find sum of odd numbers from 1 to 10 using for statement -
+
+/*#include<stdio.h>
+void main()
+{
+    int i;
+    int sum=0;
+    for(i=1; i<=10; i++)
+    {
+        if(i%2!=0)
+    
+    {
+       sum=sum+i;
+    }
+  
+}
+ printf("%d\n",sum);
+}*/
+
+//Q11. program to find sum of even and odd numbers from 1 to 10 statement -
+
+/*#include<stdio.h>
+void main()
+{
+    int i;
+    int sumEven=0;
+    int sumOdd=0;
+    for(i=1; i<=10; i++)
+    {
+        if(i%2==0 )
+    
+    {
+       sumEven=sumEven+i;
+    }
+    else{
+        sumOdd = sumOdd+1;
+    }
+  
+}
+ printf("sum of even numbers : %d\n",sumEven);
+ printf("sum of odd numbers : %d\n",sumOdd);
+}*/
+
+//Q12. program to input two numbers a and b and display numbers between them using for statement - Code
+
+/*#include<stdio.h>
+void main()
+{
+    int a,b,i;
+    printf("Enter the first number : ");
+    scanf("%d",&a);
+    printf("Enter the second number : ");
+    scanf("%d",&b);
+    
+    for(i=a+1; i<b; i++)
+    {
+        printf("%d",i);
+    }
+}*/
+
+//Q13. program to input two numbers a and b and find sum of numbers between them using for statement -
+
+
+/*#include<stdio.h>
+void main()
+{
+    int sum=0;
+    int a,b,i;
+    printf("Enter the first number : ");
+    scanf("%d",&a);
+    printf("Enter the second number : ");
+    scanf("%d",&b);
+    sum = sum+1;
+    for(i=a; i<b; i++)
+    {
+            sum = sum+1;
+    }
+        printf("The sum is : %d\n",sum);
+    
+}*/
+
+//Q14. program to find factorial of a number using for statement - Code
+ /*#include<stdio.h>
+ void main()
+ {
+    int i, n=5, fact = 1;
+    for(i=n; i>=1; i--)
+    {
+        fact = fact*i;
+    }
+    printf("factorial = %d",fact);
+ }*/
--- swtch.c
+++ swtch.c
@@ -118,7 +118,7 @@ void main()
 
 //Q14. program to demonstrate switch-case statement example 1, displaying day of the week-
 
-#include<stdio.h>
+/*#include<stdio.h>
 void main()
 {
     int choice =5;
@@ -149,4 +149,215 @@ void main()
         printf("wrong choice");
         
     }
-}
+}*/
+
+//Q15. program to demonstrate switch-case example 2,displaying month of the year-
+
+/*#include<stdio.h>
+void main()
+{
+    int choice =8;
+    switch(choice)
+    {
+        case 1:
+        printf("january");
+        break;
+        case 2:
+        printf("Feburary");
+        break;
+        case 3:
+        printf("March");
+        break;
+        case 4:
+        printf("April");
+        break;
+        case 5:
+        printf("May");
+        break;
+        case 6:
+        printf("June");
+        break;
+        case 7:
+        printf("July");
+        break;
+        case 8:
+        printf("August");
+        break;
+        case 9:
+        printf("September");
+        break;
+        case 10:
+        printf("Octover");
+        break;
+        case 11:
+        printf("November");
+        break;
+        case 12:
+        printf("December");
+        break;
+        default :
+        printf("wrong choice");
+        
+    }
+}*/
+
+//Q4. program to input three numbers and find smallest of three numbers using switch statement-
+
+
+
+/*#include<stdio.h>
+void main()
+{
+
+    int a,b,c,choice;
+    printf("press 1 find the three greater \n press  2 find the three less\n enter the user choice");
+     scanf("%d",&choice);
+    printf("enter the three integer data");
+    scanf("%d%d%d",&a,&b,&c);
+   switch (choice)
+   {
+   case 1:
+      if(a>b && a>c)
+      printf("a is greater then b and c");
+      else if(b>a && b>c)
+      printf("b is greater then a and c");
+      else if(c>a && c>b)
+      printf("c is greater then a and c");
+      else 
+      printf("invalid input");
+    break;
+    case 2:
+    if(a<b && a<c)
+      printf("a is less then b and c");
+      else if(b<a && b<c)
+      printf("b is less then a and c");
+      else if(c<a && c<b)
+      printf("c is less then a and c");
+      else 
+      printf("invalid input");
+      break;
+   
+   default:
+   printf("Does not match your choice");
+    break;
+   }
+}*/
+
+/*Q5. program to demonstrate switch statement and (check whether marks are
+     between 80 and 90) marks>80 && marks<90*/
+
+     /*#include<stdio.h>
+     void main()
+     {
+        int choice;
+        printf("press 1 marks are between 80 & 90  \n press 2 marks are not between 80 & 90 \n Enter the user choice:");
+        scanf("%d",&choice);
+        float marks;
+        printf("Enter the marks : ");
+        scanf("%f",&marks);
+        switch(choice)
+        {
+            case 1:
+            if(marks>=80 && marks<=90)
+            printf("marks are between 80 and 90");
+            else
+             printf("marks are not between 80 and 90");
+            break;
+            case 2:
+            if(marks<80 || marks>90)
+            printf("marks are not between 80 and 90");
+            else
+            printf("Actually,marks are between 80 and 90");
+            break;
+            default:
+            printf("not matched");
+        }
+     }*/
+
+//Q7. program to input a character and check whether it is equal to a or b using switch statement example of OR( || ) operator .
+   
+/*#include<stdio.h>
+void main()
+{
+    char ch;
+    char choice;                                                wrong answer
+     printf("enter the user choice ");
+     scanf("%c",&choice);
+  
+     switch (choice)
+     {
+     case 'a':
+       printf("enter the character ");
+     scanf("%c",&ch);
+        if(ch=='a' || ch=='b')
+        printf("character are equal to a and b");
+        else
+        printf("character is not equal to a and b");
+        break;
+    
+     default:
+     printf("does not esixt user choice");
+        break;
+     }
+}*/
+
+
+//Q20. program to convert fahrenheit temp to celsius and vice versa using switch statement-
+
+/*#include<stdio.h>
+void main()
+{
+    int choice;
+    float temp,convertedTemp;
+    printf("Temperature conversion :/n");
+    printf("Fahrenheit to celsius :/n");
+    printf("Celsius to fahrenheit :/n");
+    printf("Enter your choice (1 or 2):\n ");
+    scanf("%d",&choice);
+    switch(choice)
+    {
+        case 1:
+        printf("Enter temperature in Fahrenheit\n: ");
+        scanf("%d",&temp);
+        convertedTemp = (temp-32)*5/9;
+        printf("%f  Fahrenheit is %f Celsius\n",temp,convertedTemp);
+        break;
+
+        case 2:
+        printf("Enter temperature in Celsius\n: ");
+        scanf("%d",&temp);
+        convertedTemp = (temp*9/5)+32;
+        printf("%f  Celsius is %f Fahrenheit\n",temp,convertedTemp);
+        break;
+        
+        default:
+        printf("Invalid choice");
+    }
+}*/
+
+//21. program to input a character and check whether it is a vowel or not using switch statement-code
+
+/*#include<stdio.h>
+void main()
+{
+    int choice;
+    printf("Enter user choice:");
+    scanf("%d",&choice);
+    char ch;
+    printf("Enter any character:\n");
+    scanf("%c",&ch);
+    
+
+    switch(choice)
+    {                                                                         wrong
+        case 1:
+        if(ch=='a' || ch == 'e' || ch=='i' || ch == 'o' || ch == 'u')
+        printf(%c is vovel\n",ch);
+        else
+        printf("%c is not vovel\n",ch);
+
+ 
+        default:
+        printf("Invalid entry\n");
+    }
+        }*/
